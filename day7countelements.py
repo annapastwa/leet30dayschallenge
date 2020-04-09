c46573eda19c69ea8fd1dbe1e7d793b4cc838ca3
Anna Pastwa <pastwa.anna@gmail.com>
2020-04-09 21:34:31 +0200

    Update readme

--- day7countelements.py
+++ day7countelements.py
@@ -1,6 +1,7 @@
 # Given an integer array arr, count element x such that x + 1 is also in arr.
 # If there're duplicates in arr, count them seperately.
 
+
 class Solution(object):
     def countElements(self, arr):
         """
